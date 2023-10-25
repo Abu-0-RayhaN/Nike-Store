bd22d97cb8b4ba1e48d2c8e69618005e486b024d
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-25 23:10:36 +0600

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 // import React from 'react'
-import { CustomerReviews, Footer,Hero,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality } from "./sections"
-import Nav from "./components/Nav"
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+import Nav from "./components/Nav";
 const App = () => {
   return (
     <main className="relative">
-      <Nav/>
-      <section className="xl:padding-1 wide:padding-r padding-b">
-       <Hero/>
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
       </section>
       <section className="padding">
-        <PopularProducts/>
+        <PopularProducts />
       </section>
       <section className="padding">
-        <SuperQuality/>
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        <Services/>
+        <Services />
       </section>
       <section className="padding">
-        <SpecialOffer/>
+        <SpecialOffer />
       </section>
       <section className="padding bg-pale-blue">
-        <CustomerReviews/>
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe/>
+        <Subscribe />
       </section>
       <section className="padding bg-black padding-x padding-t pb-8 text-white">
-        <Footer/>
+        <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
