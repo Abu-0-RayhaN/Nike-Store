bab834d22fa6bf7b10340443d2d3b1fe95558229
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-26 21:35:04 +0600

    popular product final

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -3,19 +3,19 @@ import { products } from "../constants";
 const PopularProducts = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
-      <div className="flex flex-col justify-start gap-5">
-        <h2 className="text-4xl font-palanquin font-bold">
-          our <span className="text-coral-red">Popular</span> Porducts
+      <div className="flex flex-col justify-center gap-5">
+        <h2 className="text-4xl font-palanquin font-bold ">
+          our <span className="text-coral-red ">Popular</span> Porducts
         </h2>
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notch quality and style with our sough-after
           selections. Discover a world of comfort, design, and value.
         </p>
       </div>
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14 sm:gap-4">
-        {products.map((product) => {
-          <PopularProductCard key={product.name} />;
-        })}
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14 sm:gap-4 mx-[20%] sm:mx-auto">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );
